a01622b5b5b484cc0675d6333961583d2ca92c7c
Helen Koike <helen@koikeco.de>
2022-08-29 20:21:15 -0300

    Adding lab02 files

---
+++ lab02/lab02.py
@@ -0,0 +1,19 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 2 - Rumo a Marte
+# Nome:
+# RA:
+###################################################
+
+# Leitura de dados
+
+
+
+
+
+# Cálculo do tempo total gasto por cada espaçonave 
+
+
+
+# Impressão da resposta
+
