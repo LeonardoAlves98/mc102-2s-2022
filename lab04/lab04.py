7ca1698f20539a466c56a83ef3452d88f468261d
Helen Koike <helen@koikeco.de>
2022-09-19 06:26:52 -0300

    Adding lab04 files and ci tests

---
+++ lab04/lab04.py
@@ -0,0 +1,11 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 4 - Controle de Estoque
+# Nome:
+# RA:
+###################################################
+
+# leitura da sequência de compras e vendas
+
+
+# impressão da saída
