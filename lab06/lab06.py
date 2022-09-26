c4524a11d9cdee3aeeb7304ae2cf7e073988e3e2
Helen Koike <helen@koikeco.de>
2022-09-26 20:42:08 -0300

    Adding lab06 files and ci tests

---
+++ lab06/lab06.py
@@ -0,0 +1,14 @@
+##################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 6 - Torre de Panquecas
+# Nome:
+# RA:
+##################################################
+
+# Leitura da torre de panquecas
+torre = [int(panqueca) for panqueca in input().split()]
+
+# Leitura e processamento dos movimentos
+
+
+# Impressão da saída
