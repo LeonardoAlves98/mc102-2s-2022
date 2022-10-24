f66847fe8addabbd8684be86fca9350da87649b2
Leonardo Alves <leonardo.a_silva@hotmail.com>
2022-10-24 20:41:12 -0300

    new lab02.py Tentativa 04

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -6,14 +6,18 @@
 ###################################################
 
 # Leitura de dados
+d1 = int(input(""))
+v1 = int(input(""))
+t = int(input(""))
+d2 = int(input(""))
+v2 = int(input(""))
+# Cálculo do tempo total gasto por cada espaçonave
+space = d1 / v1
+blue = d2 / (v2 * t)
 
-
-
-
-
-# Cálculo do tempo total gasto por cada espaçonave 
-
-
-
+if space < blue:
+    chegou = True
+else:
+    chegou = False
 # Impressão da resposta
-
+print(chegou)
