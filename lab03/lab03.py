45ef92d806fa16f0352b83d542be22b1903d5094
Leonardo Alves <leonardo.a_silva@hotmail.com>
2022-12-19 20:27:48 -0300

    new lab03.py (Troca de int para str, or e and)

--- lab03/lab03.py
+++ lab03/lab03.py
@@ -7,15 +7,51 @@
 
 # leitura de dados
 
-
+Dia = int(input(': '))
+Hora = str(input(': '))
+Minuto = str(input(': '))
+Estudante = str(input(':'))
+Forma_pag = str(input(':'))
 # valor do ingresso inteiro
+ingresso = 0
+sessao = 1829
+periodo = Hora + Minuto
 
-
-
+if int(periodo) <= sessao:
+    if Dia == 1 or Dia == 7:
+        ingresso = 30
+    elif Dia == 2 or Dia == 3 or Dia == 4:
+        ingresso = 15
+    elif Dia == 5 or Dia == 6:
+        ingresso = 20
+else:
+    int(periodo) >= sessao
+    if Dia == 1 or Dia == 4 or Dia == 5:
+        ingresso = 30
+    elif Dia == 2 or Dia == 3:
+        ingresso = 20
+    elif Dia == 6 or Dia == 7:
+        ingresso = 40
 
 # valor a pagar
+if Estudante == 'N' and  Forma_pag == 'C':
+    if int(periodo) <= sessao:
+        if Dia == 1:
+            ingresso = ingresso * 0.7
+        elif Dia == 2 or Dia == 3 or Dia == 4 or Dia == 5 or Dia == 6:
+            ingresso = ingresso * 0.5
+        elif Dia == 7:
+            ingresso = ingresso * 0.8
+    else:
+        int(periodo) >= sessao
+        if Dia == 1 or Dia == 6:
+            ingresso = ingresso * 0.7
+        elif Dia == 2 or Dia == 3 or Dia == 4 or Dia == 5:
+            ingresso = ingresso * 0.5
+        elif Dia == 7:
+            ingresso = ingresso * 0.8
 
-
-
+elif Estudante == 'S':
+    ingresso = ingresso * 0.5
 # saída do valor do ingresso
 print('Valor do ingresso: R$', format(ingresso, '.2f').replace('.', ','))
