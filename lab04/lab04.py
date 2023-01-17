56621e8585d5eaf35fa9fd5d8daa5f7bc3ec6240
Leonardo Alves <leonardo.a_silva@hotmail.com>
2023-01-17 20:02:18 -0300

    new lab04.py

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -6,6 +6,25 @@
 ###################################################
 
 # leitura da sequência de compras e vendas
-
-
+estoque = unidade = vendas = 0
+while True:
+    num = int(input(':'))
+    if num != 0:
+        if num > 0:
+            estoque += num
+        elif num < 0:
+            if estoque > 0:
+                if (estoque + num) < 0:
+                    unidade = num
+                    print(f'Quantidade indiponível para a venda {unidade} unidades.')
+                else:
+                    estoque += num
+                    vendas +=1
+            else:
+                unidade = num
+                print(f'Quantidade indiponível para a venda {unidade} unidades.')
+    else:
+        break
 # impressão da saída
+print(f'Quatidade de vendas realizdas: {vendas}')
+print(f'Quantidade do estoque: {estoque}')
