fdb0af2fb161384f7901264525fdf109ce71e13a
Leonardo Alves <leonardo.a_silva@hotmail.com>
2023-02-07 07:38:42 -0300

    new laby06.py

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -9,6 +9,25 @@
 torre = [int(panqueca) for panqueca in input().split()]
 
 # Leitura e processamento dos movimentos
-
-
+l = []
+while True:
+    m = int(input())
+    count = 0
+    l.clear
+    if m > 0:
+        l = torre[:m]
+        l.reverse()
+        for x in l:
+            torre[count] = x
+            count += 1
+    else:
+        break
+next = 1
+for x in range(len(torre)-1):
+    if x < torre[next]:
+        next +=1
 # Impressão da saída
+if next == len(torre):
+    print('Torre estavel')
+else:
+    print('Torre instavel')
